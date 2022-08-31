668581fd1467176597a8a9512f047f8ec85d781f
ua.karnilchyk <v.kornilchik2015@gmail.com>
2022-08-31 10:43:31 +0300

    refactoring Date

--- src/Lesson_6/RegisterTask/Main.java
+++ src/Lesson_6/RegisterTask/Main.java
@@ -1,13 +1,15 @@
 package Lesson_6.RegisterTask;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
 
-    public static void main(String[] args) {
-        PostavkaTovarov postavkaTovarov = new PostavkaTovarov(111, "Мышь", 25, new Date());
-        ContractWithEmployer contractWithEmployer = new ContractWithEmployer(222, new Date(), new Date(), "Aslan");
-        FinanceInvoice financeInvoice = new FinanceInvoice(333, new Date(), 2456.56, 99);
+    public static void main(String[] args) throws ParseException {
+        PostavkaTovarov postavkaTovarov = new PostavkaTovarov(111, "Мышь", 25,new SimpleDateFormat("dd-MM-yyyy").parse("10-06-2020"));
+        ContractWithEmployer contractWithEmployer = new ContractWithEmployer(222, new SimpleDateFormat("dd.MM.yyyy").parse("02.04.2014"), new SimpleDateFormat("dd.MM.yyyy").parse("25.11.2017"), "Aslan");
+        FinanceInvoice financeInvoice = new FinanceInvoice(333, new SimpleDateFormat("dd.MM.yyyy").parse("10.06.2020"), 2456.56, 99);
 
         Register.printInformation(postavkaTovarov);
         Register.printInformation(contractWithEmployer);
